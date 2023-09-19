6872536661367718363b2fa2a72ac4b3b7cb32b0
Mike Miller <mikej.miller440@gmail.com>
2023-09-19 14:35:24 -0500

    added contact field to pdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,31 @@
+import { PDFViewer } from "@react-pdf/renderer";
+import { Page, Text, View, Document } from "@react-pdf/renderer";
+
 export default function App() {
-  return <div className="h-screen bg-red-600"></div>
+  return (
+    <div className="h-screen bg-red-600">
+      <PDFViewer className="h-3/4 w-screen">
+        <Document>
+          <Page style={{ fontFamily: "Times-Roman", fontSize: "12px" }}>
+            <View style={{ margin: "96px", gap: "24px" }}>
+              <View id="title">
+                <Text style={{ fontSize: "48px" }}>INVOICE</Text>
+              </View>
+              {/* Name and Date */}
+              <View>
+                <Text>Name, Last Name</Text>
+                <Text>Date</Text>
+              </View>
+              {/* Contact info */}
+              <View id="contact-info">
+                <Text>Address</Text>
+                <Text>City</Text>
+                <Text>Phone</Text>
+              </View>
+            </View>
+          </Page>
+        </Document>
+      </PDFViewer>
+    </div>
+  );
 }
