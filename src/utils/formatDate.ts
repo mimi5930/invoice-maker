4e0bc2177e57771851e63d93e675d5eb55477aaa
Mike Miller <mikej.miller440@gmail.com>
2023-09-21 13:27:08 -0500

    changed type of data from day input

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,5 +1,34 @@
+import { type Value } from "react-multi-date-picker";
+
+type VerifiedValue = NonNullable<Value>;
 
 export function formatDate(date: Date) {
-    return date.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) 
+  return date.toLocaleDateString("en-us", {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
 }
 
+export function createDateObjects(
+  dateDateObject: VerifiedValue,
+  rehearsalDateObject: VerifiedValue,
+  performanceDateObject: VerifiedValue
+) {
+  const date = new Date(dateDateObject.toString());
+  const rehearsalDateArr = rehearsalDateObject
+    .toString()
+    .split(",")
+    .map((currentDate) => {
+      return new Date(currentDate);
+    });
+  const performanceDateArr = performanceDateObject
+    .toString()
+    .split(",")
+    .map((currentDate) => {
+      return new Date(currentDate);
+    });
+
+  return { date, rehearsalDateArr, performanceDateArr };
+}
