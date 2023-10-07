93da55d82608be1f4e01dbcc39a23f2830e9d280
Mike Miller <mikej.miller440@gmail.com>
2023-10-06 20:38:41 -0500

    cleaned up unused function

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,7 +1,3 @@
-import type { Value } from "react-multi-date-picker";
-
-type VerifiedValue = NonNullable<Value>;
-
 export function formatDate(date: Date) {
   return date.toLocaleDateString("en-us", {
     weekday: "long",
@@ -9,26 +5,4 @@ export function formatDate(date: Date) {
     month: "short",
     day: "numeric",
   });
-}
-
-export function createDateObjects(
-  dateDateObject: VerifiedValue,
-  rehearsalDateObject: VerifiedValue,
-  performanceDateObject: VerifiedValue,
-) {
-  const date = new Date(dateDateObject.toString());
-  const rehearsalDateArr = rehearsalDateObject
-    .toString()
-    .split(",")
-    .map((currentDate) => {
-      return new Date(currentDate);
-    });
-  const performanceDateArr = performanceDateObject
-    .toString()
-    .split(",")
-    .map((currentDate) => {
-      return new Date(currentDate);
-    });
-
-  return { date, rehearsalDateArr, performanceDateArr };
 }
