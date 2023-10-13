e7e2f00d4fd57c14c24135fc872b87e390bfef9f
Mike Miller <mikej.miller440@gmail.com>
2023-10-13 16:20:56 -0500

    added field for additional services

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,11 +1,19 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import FormInput from "./FormInput";
 import { formSchema, type FormData } from "./formSchema";
 import DatePicker from "react-multi-date-picker";
 import DatePanel from "react-multi-date-picker/plugins/date_panel";
 import Invoice from "../document/Invoice";
 import { PDFDownloadLink } from "@react-pdf/renderer";
+import { Down, Up } from "../svg/svg";
+import { useState } from "react";
+
+type ServiceArray = {
+  description: string;
+  dates: Date[];
+  rate: number;
+};
 
 const datePickerStyles: React.CSSProperties = {
   width: "100%",
@@ -25,11 +33,19 @@ const datePickerStyles: React.CSSProperties = {
 export default function Form() {
   const {
     register,
+    control,
     handleSubmit,
     setValue,
     formState: { errors, isValid },
     getValues,
   } = useForm<FormData>({ resolver: zodResolver(formSchema) });
+
+  const { fields, append, remove } = useFieldArray({
+    control, // control props comes from useForm (optional: if you are using FormContext)
+    name: "extraFields", // unique name for your Field Array
+  });
+
+  const [serviceArray, setServiceArray] = useState<ServiceArray[]>([]);
 
   return (
     <form
@@ -169,6 +185,101 @@ export default function Form() {
       {errors.performanceDates && (
         <p className="text-red-600">{errors.performanceDates?.message}</p>
       )}
+      {fields.map((field, index) => {
+        return (
+          <div
+            key={field.id}
+            className="m-4 rounded border border-[#e0e0e0] p-4"
+          >
+            <h2 className="mb-3 text-base font-medium text-[#07074D] dark:text-slate-200">
+              Additional Service
+            </h2>
+            <div className="w-full">
+              <label
+                className="mb-1 block text-base font-medium text-[#07074D] dark:text-slate-200"
+                htmlFor="description"
+              >
+                Service Description:
+              </label>
+              <input
+                className="h-7 w-full rounded-md border border-[#e0e0e0] bg-white py-3 pl-2 text-base outline-none focus:border-[#6A64F1] focus:shadow-xl"
+                id="description"
+                {...register(`extraFields.${index}.description`)}
+              />
+              {/* {errors[`extraFields.${index}.description`] && (
+                  <p className="text-red-600 ">
+                    {
+                      errors[`extraFields.${serviceFieldIndex}.description`]
+                        ?.message
+                    }
+                  </p>
+                )} */}
+            </div>
+            <h2 className="my-1 block text-base font-medium text-[#07074D] dark:text-slate-200">
+              Service Dates
+            </h2>
+            <DatePicker
+              multiple
+              onChange={(dates) => {
+                if (dates) {
+                  setValue(
+                    `extraFields.${index}.dates`,
+                    dates
+                      .toString()
+                      .split(",")
+                      .map((currentDate) => {
+                        return new Date(currentDate);
+                      }),
+                    { shouldValidate: true },
+                  );
+                }
+              }}
+              plugins={[<DatePanel />]}
+              format="MM/DD/YYYY"
+              style={datePickerStyles}
+            ></DatePicker>
+            <div className="w-full">
+              <label
+                className="mb-1 block text-base font-medium text-[#07074D] dark:text-slate-200"
+                htmlFor="rate"
+              >
+                Service Rate
+              </label>
+              <input
+                className="mb-1 h-7 w-full rounded-md border border-[#e0e0e0] bg-white py-3 pl-2 text-base outline-none focus:border-[#6A64F1] focus:shadow-xl"
+                id="rate"
+                type="number"
+                {...register(`extraFields.${index}.rate`)}
+              />
+
+              {/* {errors.extraFields[index]?.rate && (
+                  <p className="text-red-600 ">
+                    {errors.extraFields?[index]?.rate?.message}
+                  </p>
+                )} */}
+            </div>
+            <button
+              className="mb-2 flex align-middle text-xs hover:font-semibold dark:text-slate-200"
+              onClick={() => {
+                remove(index);
+              }}
+            >
+              remove service
+              <Up />
+            </button>
+          </div>
+        );
+      })}
+
+      <button
+        className="mb-2 flex align-middle text-xs hover:font-semibold dark:text-slate-200"
+        onClick={() => {
+          append({ description: "", dates: [], rate: 0 });
+        }}
+      >
+        add a service
+        <Down />
+      </button>
       <button
         type="submit"
         className="inline-block rounded bg-blue-400 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-blue-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-blue-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-blue-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
---
+++ src/components/svg/svg.tsx
@@ -0,0 +1,35 @@
+export function Down() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="12"
+      height="12"
+      fill="currentColor"
+      viewBox="0 0 16 16"
+      className="ml-1 h-4 dark:text-slate-200"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+      />
+    </svg>
+  );
+}
+
+export function Up() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="12"
+      height="12"
+      fill="currentColor"
+      className="ml-1 h-4 dark:text-slate-200"
+      viewBox="0 0 16 16"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
+      />
+    </svg>
+  );
+}
