93c4db36ccbf962abfc80d7fb76be22ef18ac92b
Mike Miller <mikej.miller440@gmail.com>
2023-10-15 18:03:24 -0500

    fixed fillRule error

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import Form from "./components/form/Form";
 
 export default function App() {
   return (
-    <body className="min-h-screen bg-gradient-to-b from-blue-300 via-green-200 to-yellow-300 dark:bg-gradient-to-b dark:from-gray-700 dark:via-gray-900 dark:to-black ">
+    <div className="min-h-screen bg-gradient-to-b from-blue-300 via-green-200 to-yellow-300 dark:bg-gradient-to-b dark:from-gray-700 dark:via-gray-900 dark:to-black ">
       <h1 className="py-10 text-center text-5xl font-bold dark:text-slate-200">
         Invoice Maker
       </h1>
       <Form />
-    </body>
+    </div>
   );
 }
--- src/components/svg/svg.tsx
+++ src/components/svg/svg.tsx
@@ -9,7 +9,7 @@ export function Down() {
       className="ml-1 h-4 dark:text-slate-200"
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
       />
     </svg>
@@ -27,7 +27,7 @@ export function Up() {
       viewBox="0 0 16 16"
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
       />
     </svg>
